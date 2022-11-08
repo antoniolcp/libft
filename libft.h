252cf6cc525d996be9324aef687952e009cfd751
Antonio Pedro Lamy Carneiro Collares Pereira <alamy-ca@c1r8s1.42lisboa.com>
2022-11-08 18:10:10 +0000

    Alamy e uma outa

---
+++ libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alamy-ca <alamy-ca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 15:51:37 by alamy-ca          #+#    #+#             */
+/*   Updated: 2022/11/08 16:58:48 by alamy-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int ft_isalpha(int a);
+int	ft_isdigit(int a);
+int	ft_isalnum(int a);
+
+#endif
